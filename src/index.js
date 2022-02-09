7952186af0d5c9ca876284a2c94f442725dd9150
Serafim <7620226@gmail.com>
2022-02-09 03:59:03 +0300

    fix: minor fixes

--- src/index.js
+++ src/index.js
@@ -8,9 +8,6 @@ module.exports = function check(str, bracketsConfig) {
     bracketsPairs[element[0]] = element[1];
   });
 
-  // console.log(openBrackets);
-  // console.log(bracketsPairs);
-
   for (let i = 0; i < str.length; i++) {
     let currentBracket = str[i];
     if (
@@ -20,15 +17,11 @@ module.exports = function check(str, bracketsConfig) {
       stack.push(currentBracket);
     } else {
       if (stack.length === 0) {
-        // console.log("here1");
         return false;
       }
 
       let lastBracket = stack[stack.length - 1];
-      // console.log(i);
-      // console.log(currentBracket);
-      // console.log(bracketsPairs[currentBracket]);
-      // console.log(lastBracket);
+
       if (
         bracketsPairs[lastBracket] === currentBracket ||
         currentBracket === "|"
@@ -38,6 +31,5 @@ module.exports = function check(str, bracketsConfig) {
     }
   }
 
-  // console.log("here2");
   return stack.length === 0;
 };
